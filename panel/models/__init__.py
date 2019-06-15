1c077eb63efac545c17da49f4b960e87c31744cd
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-06-15 16:44:00 -0300

    Register model abstence_permission

--- panel/models/__init__.py
+++ panel/models/__init__.py
@@ -1,1 +1,2 @@
 from panel.models.customer import *  # noqa: F403, F401
+from panel.models.abstence_permission import *  # noqa: F403, F401
---
+++ panel/models/abstence_permission.py
@@ -0,0 +1,18 @@
+from django.db import models
+from panel.models import Customer
+
+class AbstencePermission(models.Model):
+    class Meta:
+        verbose_name = 'AbstencePermission: AbstencePermission'
+
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    hours_absence = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return 'AbstencePermission #{}: {}'.format(self.id, self.name)
