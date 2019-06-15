3ded8d34f886abd793d318a4e9b759cc379f290d
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-06-15 16:42:59 -0300

    Register models in admin

--- panel/admin.py
+++ panel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from panel.models import Customer, AbstencePermission
 # Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+
+class AbstencePermissionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(AbstencePermission, AbstencePermissionAdmin)
